60167b9b8f228493484914154e4e661607f11ac7
Ryan Abis <ryan.abis@gmail.com>
2018-08-20 11:43:17 +0800

    feat(Timelines): added Timeline component

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 // import TradingView from '../components/TradingView'
-
+import Timelines from '../components/Timelines'
 
 class Containers extends Component {
   render() {
     return (
       <div>
-        test
+        <Timelines />
       </div>
     );
   }
