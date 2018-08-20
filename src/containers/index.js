e7ec44b58e77947d399b54524d33b098ac7f9bed
Ryan Abis <ryan.abis@gmail.com>
2018-08-20 10:23:01 +0800

    style(app): folder structure

---
+++ src/containers/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Containers extends Component {
+  render() {
+    return (
+      <div>
+        test
+      </div>
+    );
+  }
+}
+
+export default Containers;
