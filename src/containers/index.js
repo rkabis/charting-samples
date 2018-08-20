77e576583540418e0291c20979a87cf025535aaa
Ryan Abis <ryan.abis@gmail.com>
2018-08-20 12:21:26 +0800

    feat(HighStock): added HighStock component

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 // import TradingView from '../components/TradingView'
 import Timelines from '../components/Timelines'
+import HighStock from '../components/HighStock'
 
 class Containers extends Component {
   render() {
     return (
       <div>
+        <HighStock />
         <Timelines />
       </div>
     );
