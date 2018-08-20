845fb82cc6bc35ee37e78348da13c6e30916e62a
Ryan Abis <ryan.abis@gmail.com>
2018-08-20 17:30:23 +0800

    feat(Timelines): use graphql block data

--- src/containers/index.js
+++ src/containers/index.js
@@ -4,12 +4,39 @@ import React, { Component } from 'react';
 import Timelines from '../components/Timelines'
 import HighStock from '../components/HighStock'
 
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+const GET_BLOCKS = gql`
+  {
+    getHistoricalBlockchainData(
+      symbol: "BTC",
+      timeRange: {
+        from: "2018-01-09T02:54:25Z",
+        to: "2018-01-19T02:54:25Z"
+      }
+    ) {
+      timestamp
+      blockCount
+    }
+  }
+`
+
 class Containers extends Component {
   render() {
     return (
-      <div>
-        <HighStock />
-        <Timelines />
+    	<div>
+	    	<HighStock />
+	    	<Query query={GET_BLOCKS}>
+		    	{({ loading, error, data }) => {
+		    		if (loading) return null;
+		    		if (error) return `Error!: ${error}`
+
+	    			return (
+	    				<Timelines data={data.getHistoricalBlockchainData} />
+    				)
+		    	}}
+	      </Query>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ import './assets/index.css';
 import Containers from './containers/';
 import registerServiceWorker from './registerServiceWorker';
 
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
 ReactDOM.render(
-	<Containers />, 
+	<ApolloProvider client={client}>
+		<Containers />
+	</ApolloProvider>,
 	document.getElementById('root')
 );
 
